e5196dc40041c3656eabbc1a74a24386bbfa40b1
Илья Рудюк <i.rudiuk1997.11.22@gmail.com>
2022-11-09 01:10:04 +0200

    added registration api

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,62 @@
+const userService = require("../service/user-service.js")
+
+class UserController {
+  async registration(req, res, next) {
+    try {
+      const { email, password } = req.body
+
+      const userData = await userService.registration(email, password)
+
+      res.cookie("refreshToken", userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true
+      })
+
+      return res.json(userData)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async login(req, res, next) {
+    try {
+
+    } catch (error) {
+
+    }
+  }
+
+  async logout(req, res, next) {
+    try {
+
+    } catch (error) {
+
+    }
+  }
+
+  async activate(req, res, next) {
+    try {
+
+    } catch (error) {
+
+    }
+  }
+
+  async refresh(req, res, next) {
+    try {
+
+    } catch (error) {
+
+    }
+  }
+
+  async getUsers(req, res, next) {
+    try {
+      res.json(["123", "452"])
+    } catch (error) {
+
+    }
+  }
+}
+
+module.exports = new UserController()
--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
+require("dotenv").config()
+
 const express = require("express")
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
+const mongoose = require("mongoose")
 
-const PORT = 8000
+const router = require("./router/index.js")
+
+const PORT = process.env.PORT || 9000
 const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+app.use("/api", router)
 
 const start = async () => {
   try {
-    app.listen(PORT, () => console.log(`Server started on PORT = ${8000}`))
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+
+    app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`))
   } catch (error) {
     console.log(error)
   }
